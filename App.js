b8431f373e7ca563c80ba96266661e516c4d4f01
robocon321 <robocon321n@gmail.com>
2021-06-23 09:41:24 +0700

    ScrollView

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import CategoryListItem from './components/CategoryListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CategoryListItem />
-      <CategoryListItem />
-      <CategoryListItem />
+      <ScrollView style={{paddingLeft:16, paddingRight:16}}>
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+        <CategoryListItem />
+      </ScrollView>
     </View>
   );
 }
