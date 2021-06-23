8685bd25e4aa78eb4a94a9afd0eec831cc23fb1e
robocon321 <robocon321n@gmail.com>
2021-06-23 23:34:56 +0700

    ImageBackground

--- App.js
+++ App.js
@@ -1,61 +1,34 @@
 import React from 'react';
-import { StyleSheet, View, Button, Text} from 'react-native';
+import { StyleSheet, View, ImageBackground, Text} from 'react-native';
 
 export default class App extends React.Component{  
+
   render(){
-    return(
-      <View style={styles.container}>
-        <View style={styles.wrap}>
-          <Text>The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.</Text>
-          <Button title="Press" style={styles.button} />
-        </View>
-        <Separate />
-        <View style={styles.wrap}>
-          <Text>Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.</Text>
-          <Button title="Press" color="#f194ff" style={styles.button}/>
-        </View>
-        <Separate />
-        <View style={styles.wrap}>
-          <Text>All interaction for the component are disabled.</Text>
-          <Button title="Press" color="#f194ff" disabled style={styles.button}/>
-        </View>
-        <Separate />
-        <View style={styles.wrap}>
-          <Text>This layout strategy lets the title define the width of the button.</Text>
-          <View  style={styles.child}>
-            <Button title="Left" />
-            <Button title="Right" />
-          </View>
-        </View>
+    const image = {uri:"https://reactjs.org/logo-og.png"}
+    return (
+      <View  style={styles.container}>
+        <ImageBackground source={image} style={styles.image}>
+          <Text  style={styles.text}>Hello world</Text>
+        </ImageBackground>
       </View>
     )
-  }  
-}
-
-const Separate = ()=>{
-  return <View style={styles.separate}/>
+  } 
 }
 
 const styles = StyleSheet.create({
-  separate:{
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth
-  },
   container:{
+    flex:1,
+    flexDirection: "column"
+  },
+  image: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    paddingHorizontal: 10
+    justifyContent:"center"
   },
-  wrap:{
-
-  },
-  button:{
-    paddingTop:50
-  },
-  child: {
-    flexDirection:"row",
-    justifyContent: "space-between"
+  text:{
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000a0"    
   }
 })
